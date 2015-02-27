029279c7263086494baef17226e4eb8971395050
Johanna Amann <johanna@icir.org>
2015-02-27 12:10:35 -0800

    move pod documentation to separate file for github

--- lib/Bro/Log/Parse.pm
+++ lib/Bro/Log/Parse.pm
@@ -12,41 +12,6 @@ our $VERSION = '0.01';
 
 #@EXPORT_OK = qw//;
 
-=head1 NAME
-
-Bro::Log::Parse - Object-oriented Perl interface for parsing Bro log files
-
-=head1 SYNOPSIS
-
-  use Bro::Log::Parse;
-  
-  my $parse = Bro::Log::Parse->new('/path/to/logfile');
-  while ( $fields = $parse->getLine() ) {
-    print $fields->{ts}."\n";
-  }
-
-=head1 ABSTRACT
-
-Perl inerface for parsing Bro logfiles
-
-=head1 DESCRIPTION
-
-This library provides an easy and convenient way to parse the log files generated
-by the L<Bro Network Monitoring System|http://www.bro.org>.
-
-=head1 FUNCTIONS
-
-=head2 new(constructor)
-
-The base constructor for Bro::Log::Parse classes. It takes a mandatory parameter containing
-the path to the logfile as an adrument and returns the new object. If no file is specified,
-input will be read from <>.
-
-If the file pointed to does not exist or cannot be opened, a fatal error is raised.
-
-=cut
-
-
 sub new {
   my $class = shift;
   my $file = shift;
@@ -102,13 +67,6 @@ sub readheader {
 }
 
 
-=head2 getLine()
-
-Read the the line of the input and return the parsed data as a hash. Returns
-undef when on EOF.
-
-=cut
-
 sub getLine {
   my $self = shift;
 
@@ -138,15 +96,5 @@ sub getLine {
     return \%f;
   }
 }
-
-=head1 AUTHOR
-Johanna Amann, E<lt>johanna@icir.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-Copyright 2014 by Johanna Amann
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
